2e46227c2960c49837a3f30a29f4860b58565e57
Herman Botshelo Ngwenya <hermanbngwenya@gmail.com>
2024-02-23 01:48:46 +0200

    header

--- ALTERNATIVE/podcast app/src/components/Header.jsx
+++ ALTERNATIVE/podcast app/src/components/Header.jsx
@@ -1,13 +1,14 @@
 
-import React from "react"
+import React from "react";
+
 
 export default function Header() {
     return (
         <header className="header">
-            {/* <img 
+            <img 
                 src="../images/microphone.png"
                 className="header--image"
-            /> */}
+            />
             <h2>Hermanthaserver-Podcasts</h2>
         </header>
     )
